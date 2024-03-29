ae23cc7b7358f18127ae4a366a0cedee854dcde3
itsS4nty <50720295+itsS4nty@users.noreply.github.com>
2024-03-29 19:00:50 +0100

    revert: axios prod url

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -4,7 +4,7 @@ import { MODE } from './constants';
 let url = 'http://localhost:3030/';
 
 if(MODE === 'staging') url = '';
-else if(MODE === 'production') url = 'https://todo-list-backend-inky.vercel.app/';
+else if(MODE === 'production') url = '';
 else if(MODE === 'alpha') url = '';
 else if(MODE === 'demo') url = '';
 
